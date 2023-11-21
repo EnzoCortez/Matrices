2c506c18ee013f9141cc7aa9ac8a75fef66c2bd0
EnzoCortez <enzo.cortez@udla.edu.ec>
2023-11-21 15:02:33 -0500

    Definicion del cuerpo del programa y la matriz

---
+++ matrices.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int n,m;
+
+int main(){
+    printf("Ingrese el numero de filas: ");
+    scanf("%d",&n);
+
+    printf("Ingrese el numero de filas: ");
+    scanf("%d",&n);
+    
+    int matriz[n][m];
+
+
+}
