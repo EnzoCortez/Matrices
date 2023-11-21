c926c425711688d6247568d9d11a48f9c752cefe
EnzoCortez <enzo.cortez@udla.edu.ec>
2023-11-21 15:15:17 -0500

    Impresion de de la matriz con la diagonal principal en 1

--- matrices.c
+++ matrices.c
@@ -1,15 +1,39 @@
 #include <stdio.h>
 
-int n,m;
+int n, m;
 
-int main(){
+int main()
+{
     printf("Ingrese el numero de filas: ");
-    scanf("%d",&n);
+    scanf("%d", &n);
 
     printf("Ingrese el numero de filas: ");
-    scanf("%d",&n);
-    
-    int matriz[n][m]=0;
+    scanf("%d", &m);
 
+    int matriz[n][m];
 
+    for (int i = 0; i < n; i++)
+    {
+
+        for (int j = 0; j < m; j++)
+        {
+            if (i == j)
+            {
+                matriz[i][j] = 1;
+            }
+            else
+            {
+                matriz[i][j] = 0;
+            }
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("\n");
+        for (int j = 0; j < m; j++)
+        {
+            printf("%d", matriz[i][j]);
+        }
+    }
 }
